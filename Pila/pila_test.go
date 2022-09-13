69aa979f2ea5c775ac39bdd32b0eb1dbaedfb6a4
Fabri <moboficial00@gmail.com>
2022-09-13 18:58:05 -0300

    Prueba de como funciona el testeo

--- Pila/pila_test.go
+++ Pila/pila_test.go
@@ -1,11 +1,13 @@
 package pila_test
 
 import (
+	"fmt"
 	TDAPila "pila"
 	"testing"
 )
 
 func TestPilaVacia(t *testing.T) {
 	pila := TDAPila.CrearPilaDinamica[int]()
-	// ...
+	fmt.Println(pila.EstaVacia())
 }
+TestPilaVacia(testing
