ca98b71110c2ebe67d1f6ca74c8c44e84abf0d91
Fabri <moboficial00@gmail.com>
2022-09-13 17:14:56 -0300

    Funcion crear pila dinamica

--- Pila/pila_dinamica.go
+++ Pila/pila_dinamica.go
@@ -7,4 +7,8 @@ type pilaDinamica[T any] struct {
 	cantidad int //Cantidad de elementos que tiene la pila
 }
 
-
+func CrearPilaDinamica[T any]() Pila[T] {
+	pila := new(pilaDinamica[T]) // ACA esta el cambio
+	// hago lo que deba hacer
+	return pila
+}
