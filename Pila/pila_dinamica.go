498751be0bc07c8aed389120078264c09a2df4f9
Fabri <moboficial00@gmail.com>
2022-09-14 23:52:22 -0300

    Estructura general del programa lista

--- Pila/pila_dinamica.go
+++ Pila/pila_dinamica.go
@@ -1,7 +1,7 @@
 package pila
 
 /* Definición del struct pila proporcionado por la cátedra. */
-const capacidadInicial = 16 //Elijo una potencia de 2 para facilitar las cuentas con enteros a futuro. Sobre todo considerando que voy a duplicar (x2) y dividir por 4 (/ 2 x 2)
+const capacidadInicial = 8 //Elijo una potencia de 2 para facilitar las cuentas con enteros a futuro. Sobre todo considerando que voy a multiplicar y dividir por dos
 
 type pilaDinamica[T any] struct {
 	datos    []T //Array de datos de cualquier tipo (todos mismo tipo)
@@ -14,9 +14,12 @@ func CrearPilaDinamica[T any]() Pila[T] {
 	return pila
 }
 
-func (pila *pilaDinamica[T]) Apilar() {
-
-
+func (pila *pilaDinamica[T]) redimensionar(cuanto string) {
+switch cuanto {
+	case "+": //El array tiene que duplicarse
+		pila.capacidad = pila.capacidad * 2
+	case "-": //El array tiene que demediarse
+		pila.capacidad = (pila.capacidad / 2)
 }
 
 
@@ -36,8 +39,18 @@ func (pila *pilaDinamica[T]) VerTope() int {
 	return pila.datos[pila.cantidad - 1] //Restamos 1 porque la cantidad siempre es mayor, ya que tambien se tiene que contar al item 0 como 1er item
 }
 
-func (pila *pilaDinamica[T]) Apilar() {
+func (pila *pilaDinamica[T]) Apilar(elem T) {
 	if pila.cantidad == pila.capacidad { //Quiero ver si la cantidad va a ser igual a la capacidad de esta funcion asi la redimensiono
 		pila.redimensionar("+")
-	} 
+	}
+	pila.datos[pila.cantidad] = elem //Asigno el elemento pasado al ultimo elemento de la lista
+	pila.cantidad += 1 //Aumento la cantidad
+}
+
+func (pila *pilaDinamica[T]) Desapilar() {
+	if pila.cantidad * 4 == pila.capacidad { //Quiero ver si se cumple la condicion de redimension para liberar memoria
+		pila.redimensionar("-")
+	}
+	// DESAPILAR
+	pila.cantidad -= 1 //Aumento la cantidad
 }
