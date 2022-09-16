19cb82321c8109ce02ff9cd76d7ae2c6bf671032
Fabri <moboficial00@gmail.com>
2022-09-16 11:26:06 -0300

    Mergeo raro

--- Pila/pila_dinamica.go
+++ Pila/pila_dinamica.go
@@ -12,19 +12,46 @@ type pilaDinamica[T any] struct {
 
 func CrearPilaDinamica[T any]() Pila[T] {
 	pila := new(pilaDinamica[T])
-	pila.datos = make([]T, capacidadInicial) //Creo el array con len=0 y cap = capacidadInicial
 	pila.cantidad = 0
 	pila.capacidad = capacidadInicial
+	pila.datos = make([]T, pila.capacidad) //Creo el array con len=0 y cap = capacidadInicial
+	//fmt.Println(pila)
 	return pila
 }
 
-//func (pila *pilaDinamica[T]) redimensionar(cuanto string) {
-//switch cuanto {
-//	case "+": //El array tiene que duplicarse
-//		pila.capacidad = pila.capacidad * 2
-//	case "-": //El array tiene que demediarse
-//		pila.capacidad = (pila.capacidad / 2)
-//}
+func (pila *pilaDinamica[T]) redimensionar(cuanto string) {
+	var redimension int //Variable que me va indicar cuanto redimensionar
+	switch cuanto {
+	case "+": //El array tiene que duplicarse
+
+		redimension = 4
+
+	case "-": //El array tiene que demediarse
+
+		redimension = 1
+	}
+	fmt.Println("REDIMENSIONAR")
+	nuevoArray := make([]T, pila.capacidad/2*redimension) //Creo un nuevo array con una distinta capacidad. Siempre dividio pila.capacidad a la mitad. Si quiero dejarla a la mitad, lo multiplico por 1 (redimension); y si quiero el doble, lo multiplico por 4 (redimension)
+	fmt.Println("ANTES")
+	fmt.Println("nuevoArray")
+	fmt.Println(nuevoArray)
+	fmt.Println("pila.datos")
+	fmt.Println(pila.datos)
+	copy(nuevoArray, pila.datos)
+	fmt.Println("DESPUES")
+	fmt.Println("nuevoArray")
+	fmt.Println(nuevoArray)
+	fmt.Println("pila.datos")
+	fmt.Println(pila.datos)
+	//fmt.Println("CAMBIO EL ARRAY DE LA PILA")
+	//fmt.Println("Antes")
+	//fmt.Println(pila)
+	//fmt.Println("Despues")
+	pila.capacidad = cap(nuevoArray)
+	pila.datos = nuevoArray
+	//fmt.Println(pila)
+
+}
 
 func (pila *pilaDinamica[T]) EstaVacia() bool {
 	switch pila.cantidad {
@@ -44,7 +71,7 @@ func (pila *pilaDinamica[T]) VerTope() T {
 
 func (pila *pilaDinamica[T]) Apilar(elem T) {
 	if pila.cantidad == pila.capacidad { //Quiero ver si la cantidad va a ser igual a la capacidad de esta funcion asi la redimensiono
-		//pila.redimensionar("+")
+		pila.redimensionar("+")
 	}
 	pila.datos[pila.cantidad] = elem //Asigno el elemento pasado al ultimo elemento de la lista
 	pila.cantidad += 1               //Aumento la cantidad
@@ -52,7 +79,8 @@ func (pila *pilaDinamica[T]) Apilar(elem T) {
 
 func (pila *pilaDinamica[T]) Desapilar() T {
 	if pila.cantidad*4 == pila.capacidad { //Quiero ver si se cumple la condicion de redimension para liberar memoria
-		//pila.redimensionar("-")
+		//fmt.Println("redimensionar")
+		pila.redimensionar("-")
 	}
 	// DESAPILAR
 	pila.cantidad -= 1 //Aumento la cantidad
