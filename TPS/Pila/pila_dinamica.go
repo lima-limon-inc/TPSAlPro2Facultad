15bd2f2623f62ad3f131c3d5ccf550635ff0c195
Fabri <moboficial00@gmail.com>
2022-09-22 14:58:22 -0300

    Correciones tp

--- TPS/Pila/pila_dinamica.go
+++ TPS/Pila/pila_dinamica.go
@@ -1,7 +1,12 @@
 package pila
 
 /* Definición del struct pila proporcionado por la cátedra. */
-const capacidadInicial = 8 //Elijo una potencia de 2 para facilitar las cuentas con enteros a futuro. Sobre todo considerando que voy a multiplicar y dividir por dos
+const (
+	_CAPACIDAD_INICIAL = 8 //Elijo una potencia de 2 para facilitar las cuentas con enteros a futuro. Sobre todo considerando que voy a multiplicar y dividir por dos
+	//Siempre dividio pila.capacidad a la mitad. Si quiero dejarla a la mitad, lo multiplico por 1 (redimension); y si quiero el doble, lo multiplico por 4 (redimension). De ahi estos dos valores
+	_REDIMENSION_MAYOR = 4 //Esto equivale al doble de tamano
+	_REDIMENSION_MENOR = 1 //Esto equivale a la mitad de tamano
+)
 
 type pilaDinamica[T any] struct {
 	datos     []T //Array de datos de cualquier tipo (todos mismo tipo)
@@ -12,20 +17,13 @@ type pilaDinamica[T any] struct {
 func CrearPilaDinamica[T any]() Pila[T] {
 	pila := new(pilaDinamica[T])
 	pila.cantidad = 0
-	pila.capacidad = capacidadInicial
-	pila.datos = make([]T, pila.capacidad) //Creo el array con len=0 y cap = capacidadInicial
+	pila.capacidad = _CAPACIDAD_INICIAL
+	pila.datos = make([]T, pila.capacidad) //Creo el array con len=0 y cap = _CAPACIDAD_INICIAL
 	return pila
 }
 
-func (pila *pilaDinamica[T]) redimensionar(cuanto string) {
-	var redimension int //Variable que me va indicar cuanto redimensionar
-	switch cuanto {
-	case "+": //El array tiene que duplicarse
-		redimension = 4
-	case "-": //El array tiene que demediarse
-		redimension = 1
-	}
-	nuevoArray := make([]T, pila.capacidad/2*redimension) //Creo un nuevo array con una distinta capacidad. Siempre dividio pila.capacidad a la mitad. Si quiero dejarla a la mitad, lo multiplico por 1 (redimension); y si quiero el doble, lo multiplico por 4 (redimension)
+func (pila *pilaDinamica[T]) redimensionar(cuanto int) {
+	nuevoArray := make([]T, pila.capacidad/2*cuanto) //Creo un nuevo array con una distinta capacidad.
 	copy(nuevoArray, pila.datos)
 	pila.capacidad = cap(nuevoArray)
 	pila.datos = nuevoArray
@@ -33,16 +31,11 @@ func (pila *pilaDinamica[T]) redimensionar(cuanto string) {
 }
 
 func (pila *pilaDinamica[T]) EstaVacia() bool {
-	switch pila.cantidad {
-	case 0:
-		return true
-	default:
-		return false
-	}
+	return pila.cantidad == 0 //Devuelve un true si se cumple y un false si no se cumple
 }
 
 func (pila *pilaDinamica[T]) VerTope() T {
-	if pila.cantidad == 0 {
+	if pila.EstaVacia() == true {
 		panic("La pila esta vacia")
 	}
 	return pila.datos[pila.cantidad-1] //Restamos 1 porque la cantidad siempre es mayor, ya que tambien se tiene que contar al item 0 como 1er item
@@ -50,7 +43,7 @@ func (pila *pilaDinamica[T]) VerTope() T {
 
 func (pila *pilaDinamica[T]) Apilar(elem T) {
 	if pila.cantidad == pila.capacidad { //Quiero ver si la cantidad va a ser igual a la capacidad de esta funcion asi la redimensiono
-		pila.redimensionar("+")
+		pila.redimensionar(_REDIMENSION_MAYOR)
 	}
 	pila.datos[pila.cantidad] = elem //Asigno el elemento pasado al ultimo elemento de la lista
 	pila.cantidad += 1               //Aumento la cantidad
@@ -58,9 +51,9 @@ func (pila *pilaDinamica[T]) Apilar(elem T) {
 
 func (pila *pilaDinamica[T]) Desapilar() T {
 	var tope T = pila.VerTope()            //Me guardo la variable antes de nada para paniquear en los casos de pila vacia
+	pila.cantidad -= 1                     //Aumento la cantidad
 	if pila.cantidad*4 == pila.capacidad { //Quiero ver si se cumple la condicion de redimension para liberar memoria
-		pila.redimensionar("-")
+		pila.redimensionar(_REDIMENSION_MENOR)
 	}
-	pila.cantidad -= 1 //Aumento la cantidad
 	return tope
 }
