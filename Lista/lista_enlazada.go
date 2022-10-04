91c1666fe8f6b5fca498834dd1769d5f79097b40
Fabri <moboficial00@gmail.com>
2022-10-03 22:24:30 -0300

    SUS

--- Lista/lista_enlazada.go
+++ Lista/lista_enlazada.go
@@ -1,23 +1,3 @@
-package lista
-
-type nodoLista[T any] struct {
-	dato      T
-	siguiente *nodoLista[T]
-}
-
-func nodoCrear[T any](datoArgumento T) *nodoLista[T] {
-	nodo := new(nodoLista[T])
-	nodo.dato = datoArgumento
-	nodo.prox = nil
-	return nodo
-}
-
-type listaEnlazada[T any] struct {
-	primero *nodoLista[T]
-	ultimo  *nodoLista[T]
-	largo   int
-}
-
 func CrearListaEnlazada[T any]() Lista[T] {
 	lista := new(listaEnlazada[T])
 	lista.primero = nil
